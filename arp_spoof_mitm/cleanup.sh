9abd0b52c48a21778054753ae88865c9f533c2d4
Lorenzo Rinieri <lorenzo.rinieri97@libero.it>
2021-11-15 19:27:11 +0100

    Added sudo privileges for cleanup.

--- arp_spoof_mitm/cleanup.sh
+++ arp_spoof_mitm/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker rm tcpdumper arpspoofer consolerefserver
 sudo docker rmi tcpdumper arpspoofer consolerefserver mcr.microsoft.com/dotnet/core/runtime:3.1 ubuntu
-rm -rf ./OPC\ Foundation
+sudo rm -rf ./OPC\ Foundation
